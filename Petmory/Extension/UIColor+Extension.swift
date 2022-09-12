2734f48f55ea43f69469b62094392121dfbd8289
Heecheoljjang <kkll135@naver.com>
2022-09-13 02:13:34 +0900

    placeholder  타입 프로퍼티 추가

--- Petmory/Extension/UIColor+Extension.swift
+++ Petmory/Extension/UIColor+Extension.swift
@@ -11,4 +11,5 @@ import UIKit
 extension UIColor {
     static let diaryColor = UIColor(red: 0x40/0xFF, green: 0x53/0xFF, blue: 0x36/0xFF, alpha: 1)
     static let stringColor = UIColor(red: 0xEC/0xFF, green: 0xD8/0xFF, blue: 0xCF/0xFF, alpha: 1)
+    static let placeholderColor = UIColor(red: 60/255, green: 60/255, blue: 67/255, alpha: 0.3)
 }
