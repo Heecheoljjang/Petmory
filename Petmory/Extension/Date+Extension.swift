9fca4dc7f94120a8ed4f5b5cdd421e154dcdd45c
Heecheoljjang <kkll135@naver.com>
2022-09-16 03:14:53 +0900

    Date, UIColor extension 수정

--- Petmory/Extension/Date+Extension.swift
+++ Petmory/Extension/Date+Extension.swift
@@ -7,12 +7,47 @@
 
 import Foundation
 
+enum DateFormatterType {
+    
+    case simple, full, onlyTime
+    
+    var dateFormat: String {
+        switch self {
+        case .simple:
+            return "yyyy. MM. dd"
+        case .full:
+            return "yyyy년 MM월 dd일(E) a hh:mm"
+        case .onlyTime:
+            return "a hh:mm"
+        }
+    }
+}
+
 extension Date {
-    func dateToString() -> String {
+    func dateToString(type: DateFormatterType) -> String {
         let dateFormatter = DateFormatter()
         dateFormatter.locale = Locale(identifier: "ko-KR")
-        dateFormatter.dateFormat = "yyyy. MM. dd"
+        dateFormatter.amSymbol = "오전"
+        dateFormatter.pmSymbol = "오후"
+        switch type {
+        case .simple:
+            dateFormatter.dateFormat = DateFormatterType.simple.dateFormat
+        case .full:
+            dateFormatter.dateFormat = DateFormatterType.full.dateFormat
+        case .onlyTime:
+            dateFormatter.dateFormat = DateFormatterType.onlyTime.dateFormat
+        }
         
         return dateFormatter.string(from: self)
+    }
+    func nearestHour() -> Date {
+        let calendar = Calendar.current
+        let currentMinute = calendar.component(.minute, from: self)
+        if currentMinute == 0 {
+            return calendar.date(byAdding: .minute, value: 0, to: self)!
+        } else {
+            let roundedMinute = 60 - currentMinute
+            return calendar.date(byAdding: .minute, value: roundedMinute, to: self)!
+        }
     }
 }
--- Petmory/Extension/UIColor+Extension.swift
+++ Petmory/Extension/UIColor+Extension.swift
@@ -8,17 +8,42 @@
 import Foundation
 import UIKit
 
+enum CustomColor: String {
+    case customRed, customPink, customYellow, customMint, customGreen, customBlue, customPurple, customDefault
+}
+
 extension UIColor {
     static let diaryColor = UIColor(red: 0x40/0xFF, green: 0x53/0xFF, blue: 0x36/0xFF, alpha: 1)
     static let stringColor = UIColor(red: 0xEC/0xFF, green: 0xD8/0xFF, blue: 0xCF/0xFF, alpha: 1)
     static let placeholderColor = UIColor(red: 60/255, green: 60/255, blue: 67/255, alpha: 0.3)
     
     //MARK: Tag Color
-    static let customRedColor = UIColor(red: 0xDC/0xFF, green: 0x14/0xFF, blue: 0x3D/0xFF, alpha: 1)
-    static let customPinkColor = UIColor(red: 0xFF/0xFF, green: 0xB7/0xFF, blue: 0xCB/0xFF, alpha: 1)
+    static let customRed = UIColor(red: 0xDC/0xFF, green: 0x14/0xFF, blue: 0x3D/0xFF, alpha: 1)
+    static let customPink = UIColor(red: 0xFF/0xFF, green: 0xB7/0xFF, blue: 0xCB/0xFF, alpha: 1)
     static let customPurple = UIColor(red: 0xAE/0xFF, green: 0x63/0xFF, blue: 0x9B/0xFF, alpha: 1)
     static let customGreen = UIColor(red: 0x67/0xFF, green: 0x9F/0xFF, blue: 0x58/0xFF, alpha: 1)
     static let customMint = UIColor(red: 0x76/0xFF, green: 0xEE/0xFF, blue: 0xC6/0xFF, alpha: 1)
     static let customYellow = UIColor(red: 0xEA/0xFF, green: 0xEE/0xFF, blue: 0x80/0xFF, alpha: 1)
     static let customBlue = UIColor(red: 0x5C/0xFF, green: 0x9A/0xFF, blue: 0xD1/0xFF, alpha: 1)
+    
+    static func getCustomColor(_ color: String) -> UIColor {
+        switch color {
+        case CustomColor.customRed.rawValue:
+            return UIColor.customRed
+        case CustomColor.customPink.rawValue:
+            return UIColor.customPink
+        case CustomColor.customYellow.rawValue:
+            return UIColor.customYellow
+        case CustomColor.customMint.rawValue:
+            return UIColor.customMint
+        case CustomColor.customGreen.rawValue:
+            return UIColor.customGreen
+        case CustomColor.customBlue.rawValue:
+            return UIColor.customBlue
+        case CustomColor.customPurple.rawValue:
+            return UIColor.customPurple
+        default:
+            return UIColor.diaryColor
+        }
+    }
 }
