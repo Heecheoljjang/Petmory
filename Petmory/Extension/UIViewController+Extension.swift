471e7a8ae6337f23b7b113a3f2a4d7b91992d136
Heecheoljjang <kkll135@naver.com>
2022-09-25 05:23:17 +0900

    handler있는 alert 메서드

--- Petmory/Extension/UIViewController+Extension.swift
+++ Petmory/Extension/UIViewController+Extension.swift
@@ -134,4 +134,13 @@ extension UIViewController {
         alert.addAction(ok)
         present(alert, animated: true)
     }
+    
+    func handlerAlert(title: String, message: String?, handler: ((UIAlertAction) -> Void)?) {
+        let alert = UIAlertController(title: title, message: message, preferredStyle: .alert)
+        let ok = UIAlertAction(title: "확인", style: .default, handler: handler)
+        let cancel = UIAlertAction(title: "취소", style: .cancel)
+        alert.addAction(ok)
+        alert.addAction(cancel)
+        present(alert, animated: true)
+    }
 }
