aa76aa5bc45877ed78a9e9fcd8a0abaa4ba100f2
hee <kkll135@naver.com>
2022-11-02 05:29:42 +0900

    Main화면 Rx 적용(컬렉션뷰 bind 두 번해서 생기는 오류는 해결 전)

--- Petmory/Scene/Main/Main/MainViewController.swift
+++ Petmory/Scene/Main/Main/MainViewController.swift
@@ -5,14 +5,17 @@
 //  Created by HeecheolYoon on 2022/09/09.
 //
 import UIKit
-import RealmSwift
 import FirebaseAnalytics
+import RxCocoa
+import RxSwift
 
 final class MainViewController: BaseViewController {
     
     private var mainView = MainView()
     
     let viewModel = MainViewModel()
+    
+    private let disposeBag = DisposeBag()
     
     override func loadView() {
         self.view = mainView
@@ -24,7 +27,7 @@ final class MainViewController: BaseViewController {
         
         viewModel.requestAuthorization()
 
-        //현재 년도 구해서 monthList와 더해주기
+        //현재 년도 구해서 monthList와 더해주기 위해서 현재 연도 구함
         viewModel.setCurrentYear()
 
         //백업용 텍스트파일 지우기
@@ -40,39 +43,133 @@ final class MainViewController: BaseViewController {
     override func viewWillAppear(_ animated: Bool) {
         super.viewWillAppear(animated)
         
-        viewModel.fetchAllMemory()
+        viewModel.fetchAllMemory() //task바뀜
         
-        viewModel.fetchPet()
+        viewModel.fetchPet() // petList바뀜
         
-        viewModel.setCountList()
+//        viewModel.setCountList() CountList바뀜 => fetchAllMemory되면 같이 실행됨
 
-        navigationItem.titleView = mainView.titleViewButton
+//        navigationItem.titleView = mainView.titleViewButton -> 뷰디드로드에서 currentYear바뀌면서 설정됨
     }
     
     private func bind() {
-        viewModel.currentYear.bind { [weak self] value in
-            var attributedTitle = AttributedString(value + "년")
-            attributedTitle.font = UIFont(name: CustomFont.medium, size: 16)
-            self?.mainView.titleViewButton.configuration?.attributedTitle = attributedTitle
-            
-            self?.viewModel.setTempList()
-        }
+//        viewModel.currentYear.bind { [weak self] value in
+//            var attributedTitle = AttributedString(value + "년")
+//            attributedTitle.font = UIFont(name: CustomFont.medium, size: 16)
+//            self?.mainView.titleViewButton.configuration?.attributedTitle = attributedTitle
+//
+//            self?.viewModel.setTempList()
+//        }
+//
+//        viewModel.countList.bind { [weak self] _ in
+//            self?.mainView.diaryCollectionView.reloadData()
+//        }
         
-        viewModel.countList.bind { [weak self] _ in
-            self?.mainView.diaryCollectionView.reloadData()
-        }
+        //MARK: 연도바뀜 -> templist바뀜 -> task바꾼뒤 countList구함
+        viewModel.currentYear
+            .asDriver(onErrorJustReturn: "error")
+            .drive(onNext: { [weak self] year in
+                var attributedTitle = AttributedString(year + "년")
+                attributedTitle.font = UIFont(name: CustomFont.medium, size: 16)
+                self?.mainView.titleViewButton.configuration?.attributedTitle = attributedTitle
+                self?.navigationItem.titleView = self?.mainView.titleViewButton
+                self?.viewModel.setTempList() //컬렉션뷰 날짜 스트링 만듦
+            })
+            .disposed(by: disposeBag)
+        
+        viewModel.tempList
+            .asDriver(onErrorJustReturn: [])
+            .drive(onNext: { [weak self] _ in
+                self?.viewModel.fetchAllMemory()
+            })
+            .disposed(by: disposeBag)
+        
+        viewModel.tasks
+            .asDriver(onErrorJustReturn: [])
+            .drive(onNext: { [weak self] _ in
+                self?.viewModel.setCountList()
+            })
+            .disposed(by: disposeBag)
+
+        viewModel.countList
+            .asDriver(onErrorJustReturn: [])
+            .drive(onNext: { [weak self] _ in
+                self?.mainView.diaryCollectionView.reloadData()
+            })
+            .disposed(by: disposeBag)
+        
+        mainView.writingButton.rx.tap
+            .bind { [weak self] _ in
+                self?.presentWritingView()
+            }
+            .disposed(by: disposeBag)
+        
+        mainView.titleViewButton.rx.tap
+            .bind { [weak self] _ in
+                self?.presentPickerView() //MARK: sender없어도되는지
+            }
+            .disposed(by: disposeBag)
+        
+        //피커뷰 타이틀 설정
+        viewModel.yearList
+            .map { $0.map{ "\($0)년" } }
+            .bind(to: mainView.pickerView.rx.itemTitles) { (row, element) in
+                return element
+            }
+            .disposed(by: disposeBag)
+        
+        mainView.pickerView.rx.itemSelected
+            .bind(onNext: { [weak self] row, component in
+                guard let self = self else { return }
+                
+                self.viewModel.selectedDate.accept("\(self.viewModel.yearList.value[row])")
+            })
+            .disposed(by: disposeBag)
+            
+        viewModel.tempList
+            .asDriver(onErrorJustReturn: [])
+            .drive(mainView.diaryCollectionView.rx.items(cellIdentifier: MainCollectionViewCell.identifier, cellType: MainCollectionViewCell.self)) {  (row, element, cell) in
+                
+                cell.dateLabel.text = element
+            }
+            .disposed(by: disposeBag)
+                
+        viewModel.countList
+            .do(onNext: { [weak self] _ in
+                self?.mainView.diaryCollectionView.delegate = nil
+                self?.mainView.diaryCollectionView.dataSource = nil
+            })
+            .asDriver(onErrorJustReturn: [])
+            .drive(mainView.diaryCollectionView.rx.items(cellIdentifier: MainCollectionViewCell.identifier, cellType: MainCollectionViewCell.self)) {  (row, element, cell) in
+                
+                cell.pageLabel.text = "\(element) 페이지"
+            }
+            .disposed(by: disposeBag)
+        
+        mainView.diaryCollectionView.rx.itemSelected
+            .bind(onNext: { [weak self] indexPath in
+                let monthMemoryVC = MonthMemoryViewController()
+
+                guard let monthDate = self?.viewModel.tempList.value[indexPath.item] else { return }
+                
+                monthMemoryVC.viewModel.monthDate = monthDate
+                
+                self?.transition(monthMemoryVC, transitionStyle: .push)
+            })
+            .disposed(by: disposeBag)
     }
     
     override func viewDidLayoutSubviews() {
         super.viewDidLayoutSubviews()
         
         if viewModel.isFirst.value {
-            let indexPath = IndexPath(item: Int(Date().dateToString(type: .onlyMonth))! - 1, section: 0)
+            let indexPath = viewModel.firstScrollIndex()
             mainView.diaryCollectionView.scrollToItem(at: indexPath, at: .centeredHorizontally, animated: false)
-            viewModel.isFirst.value = !viewModel.isFirst.value
+            viewModel.setIsNotFirst()
         }
     }
     
+    //MARK: - 만약 바버튼도 하면 setupController가 먼저 실행되고 바인딩
     override func setUpController() {
         
         let menuButton = UIBarButtonItem(image: UIImage(systemName: ImageName.menu), style: .plain, target: self, action: #selector(presentAllMemory))
@@ -88,13 +185,13 @@ final class MainViewController: BaseViewController {
         super.configure()
         
         mainView.diaryCollectionView.delegate = self
-        mainView.diaryCollectionView.dataSource = self
+//        mainView.diaryCollectionView.dataSource = self
         
-        mainView.writingButton.addTarget(self, action: #selector(presentWritingView), for: .touchUpInside)
-        mainView.titleViewButton.addTarget(self, action: #selector(presentPickerView(_ :)), for: .touchUpInside)
+//        mainView.writingButton.addTarget(self, action: #selector(presentWritingView), for: .touchUpInside)
+//        mainView.titleViewButton.addTarget(self, action: #selector(presentPickerView(_ :)), for: .touchUpInside)
         
-        mainView.pickerView.delegate = self
-        mainView.pickerView.dataSource = self
+//        mainView.pickerView.delegate = self
+//        mainView.pickerView.dataSource = self
         
     }
 
@@ -105,7 +202,9 @@ final class MainViewController: BaseViewController {
             
             guard let self = self else { return }
             
-            self.viewModel.currentYear.value = self.viewModel.selectedDate.value
+//            self.viewModel.currentYear.value = self.viewModel.selectedDate.value
+            self.viewModel.currentYear.accept(self.viewModel.selectedDate.value)
+            
             self.viewModel.setCountList()
         }
         let cancel = UIAlertAction(title: AlertText.cancel, style: .cancel)
@@ -113,8 +212,10 @@ final class MainViewController: BaseViewController {
         contentViewController.view = mainView.pickerView
         contentViewController.preferredContentSize.height = 200
         
-        mainView.pickerView.selectRow(Int(Date().dateToString(type: .onlyYear))! - 1990, inComponent: 0, animated: false)
-        viewModel.setSelectedDate(date: "\(Int(Date().dateToString(type: .onlyYear))!)")
+//        mainView.pickerView.selectRow(Int(Date().dateToString(type: .onlyYear))! - 1990, inComponent: 0, animated: false)
+        mainView.pickerView.selectRow(viewModel.selectedDate(count: 1990), inComponent: 0, animated: false)
+//        viewModel.setSelectedDate(date: "\(Int(Date().dateToString(type: .onlyYear))!)")
+        viewModel.setSelectedDate(date: "\(viewModel.selectedDate(count: 0))")
         
         alert.setValue(contentViewController, forKey: "contentViewController")
         alert.addAction(select)
@@ -143,54 +244,58 @@ final class MainViewController: BaseViewController {
         let settingViewController = SettingViewController()
         transition(settingViewController, transitionStyle: .push)
     }
-
-    @objc private func presentPickerView(_ sender: UIButton) {
+//
+//    @objc private func presentPickerView(_ sender: UIButton) {
+//        setDatePickerSheet()
+//    }
+    
+    @objc private func presentPickerView() {
         setDatePickerSheet()
     }
 }
 
-extension MainViewController: UICollectionViewDelegate, UICollectionViewDataSource, UICollectionViewDelegateFlowLayout {
+extension MainViewController: UICollectionViewDelegateFlowLayout {
     
-    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
-        return viewModel.monthList.count
-    }
-    
-    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
-        guard let cell = collectionView.dequeueReusableCell(withReuseIdentifier: MainCollectionViewCell.identifier, for: indexPath) as? MainCollectionViewCell else { return UICollectionViewCell() }
-        cell.dateLabel.text = viewModel.tempList.value[indexPath.item]
-        cell.pageLabel.text = "\(viewModel.countList.value[indexPath.item]) 페이지"
-        
-        return cell
-    }
-    
-    func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
-        let monthMemoryVC = MonthMemoryViewController()
-
-        monthMemoryVC.viewModel.monthDate = viewModel.tempList.value[indexPath.item]
-        
-        transition(monthMemoryVC, transitionStyle: .push)
-    }
-    
+//    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+//        return viewModel.monthList.count
+//    }
+//
+//    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+//        guard let cell = collectionView.dequeueReusableCell(withReuseIdentifier: MainCollectionViewCell.identifier, for: indexPath) as? MainCollectionViewCell else { return UICollectionViewCell() }
+//        cell.dateLabel.text = viewModel.tempList.value[indexPath.item]
+//        cell.pageLabel.text = "\(viewModel.countList.value[indexPath.item]) 페이지"
+//
+//        return cell
+//    }
+//
+//    func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
+//        let monthMemoryVC = MonthMemoryViewController()
+//
+//        monthMemoryVC.viewModel.monthDate = viewModel.tempList.value[indexPath.item]
+//
+//        transition(monthMemoryVC, transitionStyle: .push)
+//    }
+//
     func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
         return CGSize(width: 260, height: 420)
     }
 }
 
-extension MainViewController: UIPickerViewDelegate, UIPickerViewDataSource {
+//extension MainViewController: UIPickerViewDelegate, UIPickerViewDataSource {
+//
+//    func numberOfComponents(in pickerView: UIPickerView) -> Int {
+//        return 1
+//    }
+//
+//    func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
+//        return viewModel.yearList.count
+//    }
     
-    func numberOfComponents(in pickerView: UIPickerView) -> Int {
-        return 1
-    }
-    
-    func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
-        return viewModel.yearList.count
-    }
-    
-    func pickerView(_ pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
-        return "\(viewModel.yearList[row])년"
-    }
-    
-    func pickerView(_ pickerView: UIPickerView, didSelectRow row: Int, inComponent component: Int) {
-        viewModel.selectedDate.value = "\(viewModel.yearList[row])"
-    }
-}
+//    func pickerView(_ pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
+//        return "\(viewModel.yearList[row])년"
+//    }
+//
+//    func pickerView(_ pickerView: UIPickerView, didSelectRow row: Int, inComponent component: Int) {
+//        viewModel.selectedDate.value = "\(viewModel.yearList[row])"
+//    }
+//}
--- Petmory/Scene/Main/Main/MainViewModel.swift
+++ Petmory/Scene/Main/Main/MainViewModel.swift
@@ -8,6 +8,8 @@
 import Foundation
 import RealmSwift
 import UserNotifications
+import RxSwift
+import RxCocoa
 
 final class MainViewModel {
     
@@ -15,23 +17,34 @@ final class MainViewModel {
     
     let notificationCenter = UNUserNotificationCenter.current()
     
-    var tasks: Observable<Results<UserMemory>?> = Observable(nil)
+//    var tasks: Observable<Results<UserMemory>?> = Observable(nil)
+//
+//    var petList: Observable<Results<UserPet>?> = Observable(nil)
     
-    var petList: Observable<Results<UserPet>?> = Observable(nil)
+    var tasks = BehaviorRelay<[UserMemory]?>(value: [])
+    
+    var petList = BehaviorRelay<[UserPet]?>(value: [])
     
     let monthList = [". 01", ". 02", ". 03", ". 04", ". 05", ". 06", ". 07", ". 08", ". 09", ". 10", ". 11", ". 12"]
     
-    var tempList: Observable<[String]> = Observable([])
+//    var tempList: Observable<[String]> = Observable([])
+    var tempList = BehaviorRelay<[String]>(value: []) //MARK: 2022.01
+//
+//    var selectedDate: Observable<String> = Observable("")
+    var selectedDate = BehaviorRelay<String>(value: "")
+//
+//    var currentYear: Observable<String> = Observable("")
+    var currentYear = BehaviorRelay<String>(value: "") //MARK: 2022년
+//
+//    var countList: Observable<[Int]> = Observable([])
+    var countList = BehaviorRelay<[Int]>(value: []) //MARK: 각 월의 작성된 기록 수
     
-    var selectedDate: Observable<String> = Observable("")
+    let yearList = BehaviorRelay<[Int]>(value: [Int](1990...2050))
+
+//    let yearList = [Int](1990...2050)
     
-    var currentYear: Observable<String> = Observable("")
-    
-    var countList: Observable<[Int]> = Observable([])
-    
-    let yearList = [Int](1990...2050)
-    
-    var isFirst: Observable<Bool> = Observable(true)
+//    var isFirst: Observable<Bool> = Observable(true)
+    var isFirst = BehaviorRelay<Bool>(value: true)
     
     func requestAuthorization() {
         let authorizationOptions = UNAuthorizationOptions(arrayLiteral: .alert, .sound)
@@ -45,15 +58,18 @@ final class MainViewModel {
     }
     
     func setCurrentYear() {
-        currentYear.value = Date().dateToString(type: .onlyYear)
+//        currentYear.value = Date().dateToString(type: .onlyYear)
+        currentYear.accept(Date().dateToString(type: .onlyYear))
     }
     
     func fetchAllMemory() {
-        tasks.value = repository.fetchAllMemory()
+//        tasks.value = repository.fetchAllMemory()
+        tasks.accept(repository.fetchAllMemory().map { $0 })
     }
     
     func fetchPet() {
-        petList.value = repository.fetchPet()
+//        petList.value = repository.fetchPet()
+        petList.accept(repository.fetchPet().map { $0 })
     }
     
     func checkPetCount() -> Bool {
@@ -62,24 +78,50 @@ final class MainViewModel {
     
     func setCountList() {
         
-        countList.value = []
+//        countList.value = []
+        countList.accept([])
+        
+        var tempArr: [Int] = []
         
         guard let memory = tasks.value else { return }
         
         tempList.value.forEach { date in
-            countList.value.append(memory.filter("\(RealmModelColumn.memoryDateString) CONTAINS[c] '\(date)'").count)
+//            countList.value.append(memory.filter("\(RealmModelColumn.memoryDateString) CONTAINS[c] '\(date)'").count)
+            tempArr.append(memory.filter { $0.memoryDateString.contains(date) }.count)
         }
+        countList.accept(tempArr)
     }
     
     func setTempList() {
         
-        tempList.value = monthList
+//        tempList.value = monthList
+//        for i in 0..<monthList.count {
+//            tempList.value[i] = currentYear.value + monthList[i]
+//        }
+        var tempArr: [String] = []
+        
+        tempArr.append(contentsOf: monthList)
+        
         for i in 0..<monthList.count {
-            tempList.value[i] = currentYear.value + monthList[i]
+            tempArr[i] = currentYear.value + monthList[i]
         }
+        tempList.accept(tempArr)
     }
     
     func setSelectedDate(date: String) {
-        selectedDate.value = date
+//        selectedDate.value = date
+        selectedDate.accept(date)
+    }
+    
+    func firstScrollIndex() -> IndexPath {
+        return IndexPath(item: Int(Date().dateToString(type: .onlyMonth))! - 1, section: 0)
+    }
+    
+    func selectedDate(count: Int) -> Int {
+        return Int(Date().dateToString(type: .onlyYear))! - count
+    }
+    
+    func setIsNotFirst() {
+        isFirst.accept(!isFirst.value)
     }
 }
