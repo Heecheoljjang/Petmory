8952b246f3f91e8f76ce2c24bc397334ea78cb18
hee <kkll135@naver.com>
2022-11-06 18:30:28 +0900

    Photo화면 인풋아웃풋 적용, Rx 적용,  scrollViewDidScroll을 Rx의 ContentOffset으로 변경해봄

--- Petmory/Scene/Main/Photo/PhotoViewController.swift
+++ Petmory/Scene/Main/Photo/PhotoViewController.swift
@@ -6,7 +6,8 @@
 //
 
 import UIKit
-import RealmSwift
+import RxCocoa
+import RxSwift
 
 final class PhotoViewController: BaseViewController {
     
@@ -14,6 +15,8 @@ final class PhotoViewController: BaseViewController {
     
     let viewModel = PhotoViewModel()
 
+    private let disposeBag = DisposeBag()
+    
     override func loadView() {
         view = mainView
     }
@@ -26,20 +29,59 @@ final class PhotoViewController: BaseViewController {
     }
 
     private func bind() {
-        viewModel.imageList.bind { [weak self] value in
-                        
-            self?.mainView.pageControl.numberOfPages = value.count
-        }
+//        viewModel.imageList.bind { [weak self] value in
+//
+//            self?.mainView.pageControl.numberOfPages = value.count
+//        }
+        
+        let input = PhotoViewModel.Input(imageList: viewModel.imageList, collectionViewContentOffset: mainView.imageCollectionView.rx.contentOffset, tapDismiss: mainView.dismissButton.rx.tap)
+        let output = viewModel.transform(input: input)
+        
+//        viewModel.imageList
+//            .asDriver(onErrorJustReturn: [])
+        output.numberOfPages
+            .drive(onNext: { [weak self] value in
+                print("numberofpages => \(value)")
+                self?.mainView.pageControl.numberOfPages = value.count
+            })
+            .disposed(by: disposeBag)
+        
+//        viewModel.imageList
+//            .asDriver(onErrorJustReturn: [])
+        output.collectionViewItem
+            .drive(mainView.imageCollectionView.rx.items(cellIdentifier: PhotoCollectionViewCell.identifier, cellType: PhotoCollectionViewCell.self)) { row, element, cell in
+                
+                cell.photoImageView.image = UIImage(data: element)
+            }
+            .disposed(by: disposeBag)
+        
+//        mainView.imageCollectionView.rx.contentOffset
+//            .map { Double($0.x) }
+//            .disposed(by: disposeBag)
+        output.collectionViewXOffset
+            .withUnretained(self)
+            .bind(onNext: { vc, value in
+//                let width = vc.mainView.imageCollectionView.frame.width
+                guard let width = vc.mainView.window?.windowScene?.screen.bounds.width else { return }
+                print(value, width)
+                vc.mainView.pageControl.currentPage = vc.viewModel.pageControlPage(xOffset: value, width: width)
+            })
+            .disposed(by: disposeBag)
+        
+//        mainView.dismissButton.rx.tap
+        output.tapDismiss
+            .withUnretained(self)
+            .bind(onNext: { vc, _ in
+                vc.transition(vc, transitionStyle: .dismiss)
+            })
+            .disposed(by: disposeBag)
     }
     
     override func configure() {
         super.configure()
     
         mainView.imageCollectionView.delegate = self
-        mainView.imageCollectionView.dataSource = self
-        
-        mainView.dismissButton.addTarget(self, action: #selector(dismissPhotoView), for: .touchUpInside)
-        
+//        mainView.dismissButton.addTarget(self, action: #selector(dismissPhotoView), for: .touchUpInside)
     }
     
     override func setUpController() {
@@ -52,28 +94,28 @@ final class PhotoViewController: BaseViewController {
     
 }
 
-extension PhotoViewController: UICollectionViewDelegate, UICollectionViewDataSource, UICollectionViewDelegateFlowLayout {
-    
-    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
-        
-        return viewModel.fetchImageListCount()
-    }
-    
-    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
-        guard let cell = collectionView.dequeueReusableCell(withReuseIdentifier: PhotoCollectionViewCell.identifier, for: indexPath) as? PhotoCollectionViewCell else { return UICollectionViewCell() }
-
-        let imageList = viewModel.imageList.value
-        
-        cell.photoImageView.image = UIImage(data: imageList[indexPath.item])
-        
-        return cell
-    }
+extension PhotoViewController: UICollectionViewDelegateFlowLayout {
+//
+//    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+//
+//        return viewModel.fetchImageListCount()
+//    }
+//
+//    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+//        guard let cell = collectionView.dequeueReusableCell(withReuseIdentifier: PhotoCollectionViewCell.identifier, for: indexPath) as? PhotoCollectionViewCell else { return UICollectionViewCell() }
+//
+//        let imageList = viewModel.imageList.value
+//
+//        cell.photoImageView.image = UIImage(data: imageList[indexPath.item])
+//
+//        return cell
+//    }
     func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
         return CGSize(width: mainView.frame.size.width, height: mainView.frame.size.height)
     }
-    
-    func scrollViewDidScroll(_ scrollView: UIScrollView) {
-        let width = scrollView.frame.width
-        mainView.pageControl.currentPage = viewModel.pageControlPage(xOffset: scrollView.contentOffset.x, width: width)
-    }
+//
+//    func scrollViewDidScroll(_ scrollView: UIScrollView) {
+//        let width = scrollView.frame.width
+//        mainView.pageControl.currentPage = viewModel.pageControlPage(xOffset: scrollView.contentOffset.x, width: width)
+//    }
 }
--- Petmory/Scene/Main/Photo/PhotoViewModel.swift
+++ Petmory/Scene/Main/Photo/PhotoViewModel.swift
@@ -6,17 +6,39 @@
 //
 
 import Foundation
-import RealmSwift
+import RxCocoa
+import RxSwift
 
-final class PhotoViewModel {
+final class PhotoViewModel: CommonViewModel {
     
-    var imageList: MVVMObservable<[Data]> = MVVMObservable([])
-    
-    func fetchImageListCount() -> Int {
-        return imageList.value.count
+    struct Input {
+        let imageList: BehaviorRelay<[Data]>
+        let collectionViewContentOffset: ControlProperty<CGPoint>
+        let tapDismiss: ControlEvent<Void>
     }
     
-    func pageControlPage(xOffset: Double, width: CGFloat) -> Int {
+    struct Output {
+        let numberOfPages: Driver<[Data]>
+        let collectionViewItem: Driver<[Data]>
+        let collectionViewXOffset: Observable<CGFloat>
+        let tapDismiss: ControlEvent<Void>
+    }
+    
+    func transform(input: Input) -> Output {
+        let numberOfPages = input.imageList.asDriver(onErrorJustReturn: [])
+        let collectionViewItem = input.imageList.asDriver(onErrorJustReturn: [])
+        let xOffset = input.collectionViewContentOffset.map { $0.x }
+        
+        return Output(numberOfPages: numberOfPages, collectionViewItem: collectionViewItem, collectionViewXOffset: xOffset, tapDismiss: input.tapDismiss)
+    }
+    
+    var imageList = BehaviorRelay<[Data]>(value: []) //구독하기 바로 전꺼를 replay하므로 이전화면에서 데이터 전달된걸 받은 상태에서 bind되면 적용될 것 같음
+    
+    func fetchImageListCount(imageList: [Data]) -> Int {
+        return imageList.count
+    }
+
+    func pageControlPage(xOffset: CGFloat, width: CGFloat) -> Int {
         return Int(xOffset / width)
     }
 }
